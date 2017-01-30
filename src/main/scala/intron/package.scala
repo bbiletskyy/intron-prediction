9dcf815f279f2fbc318a900ab70d29aa58e0a984
Borys Biletskyy <bbiletskyy@wehkamp.nl>
2017-01-30 16:00:46 +0100

    13-As-a-researcher-I-want-to-have-a-shifting-frame-approach-implemented-to-apply-machine-learning-methods

--- src/main/scala/intron/package.scala
+++ src/main/scala/intron/package.scala
@@ -1,28 +1,39 @@
 /**
- * Domain classes
+ * Domain classes and some commonly used routines.
  */
 package object intron {
-
   case class Exon(exonId: String, geneId: String, sequence: String)
-
   case class Gene(geneId: String, exonIds: List[String] = Nil, exons: List[Exon] = Nil, sequence: String)
-
   case class Frame[X, Y](xs: List[X], y: Y) {
     assert((xs.size - 1) % 2 == 0)
-
     def radius = (xs.size - 1) / 2
   }
 
-  def toFrames(g: Gene, frameRadius: Int): Iterator[Frame[Char, Char]] = {
-    val exonSymbol = "+"
-    val intronSymbol = "-"
-    val ys = g.exons.map(_.sequence)
-      .fold(g.sequence)((geneSeq, exonSeq) => geneSeq.replace(exonSeq, exonSymbol * exonSeq.length))
-      .replaceAll(s"[^${exonSymbol}]", intronSymbol)
+  val ExonSymbol = "+"
+  val IntronSymbol = "-"
+  val FlankingSymbol = "X"
 
-    val flankingSymbol = "X"
+  def toFrames(g: Gene, frameRadius: Int): Iterator[Frame[String, String]] = {
+    val ys = g.exons.map(_.sequence)
+      .fold(g.sequence)((geneSeq, exonSeq) => geneSeq.replace(exonSeq, ExonSymbol * exonSeq.length))
+      .replaceAll(s"[^${ExonSymbol}]", IntronSymbol)
     val frameSize = frameRadius * 2 + 1
-    val xs = flankingSymbol * frameRadius + g.sequence.toUpperCase + flankingSymbol * frameRadius
-    xs.sliding(frameSize).zip(ys.iterator).map { case (xs, y) => Frame(xs.toList, y) }
+    val xs = FlankingSymbol * frameRadius + g.sequence.toUpperCase + FlankingSymbol * frameRadius
+    xs.sliding(frameSize).zip(ys.iterator).map { case (xs, y) => Frame(xs.map(_.toString).toList, y.toString) }
   }
+
+  val featureIndex: Map[String, Double] = Map(
+  "A" -> 0.0,
+  "C" -> 1.0,
+  "G" -> 2.0,
+  "T" -> 3.0,
+  "N" -> 4.0,
+  "F" -> 5.0,
+  FlankingSymbol -> 6.0
+  ).withDefaultValue(-1.0)
+
+  val labelIndex: Map[String, Double] = Map(
+    IntronSymbol -> 0.0,
+    ExonSymbol -> 1.0
+  )
 }
